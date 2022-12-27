8b1a7d950e0191dfacbfb1a75b41f6fc4f4793ff
boyinishiva <120115547+boyinishiva@users.noreply.github.com>
2022-12-28 00:21:36 +0530

    Add files via upload

---
+++ asgmt1q5.java
@@ -0,0 +1,24 @@
+
+public class asgmt1q5 {
+
+	public static void main(String[] args) {
+		int n=13;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0||i==n-1||j==0||i+j<=(n-1)/2||i-j>=(n-1)/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+			
+	}
+
+}
